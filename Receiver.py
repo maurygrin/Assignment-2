f2ba33aacef51c8657c1b0f31454b99be301448f
Mauricio Hidalgo <mrhidalgo@miners.utep.edu>
2020-10-09 15:30:02 -0600

    Assignment Finished

--- Receiver.py
+++ Receiver.py
@@ -1,37 +1,44 @@
-# receiver.py - The receiver in the reliable data transfer protocol
+# Team 16
+# Assignment 02: Reliable Data Transfer
+# October 09, 2020
+# CS5313 - Computer Networks
+
 import packet
 import socket
-import sys
 import udt
 
 RECEIVER_ADDR = ('localhost', 8080)
-
 
 # Receive packets from the sender w/ GBN protocol
 def receive_gbn(sock):
     endStr = ''
     expectedPacket = 0
 
+    # While FIN packet has not been received
     while endStr != 'END':
         pkt, senderaddr = udt.recv(sock)
-        seq, data = packet.extract(pkt)
+        SEQ, data = packet.extract(pkt)
         endStr = data.decode()
-        print("From: ", senderaddr, ", Seq# ", seq)
+        print("From: ", senderaddr, ", SEQ# ", SEQ)
 
-        if expectedPacket != seq:
-            print('This packet was not expected. Sending ACK %d' % expectedPacket)
+        # The received packet was not expected. Send ACK with the correct SEQ number
+        if expectedPacket != SEQ:
+            print('This packet was not expected. Sending ACK %d' % expectedPacket, "\n")
             pkt = packet.make(expectedPacket)
             udt.send(pkt, sock, senderaddr)
 
+        # This packet was expected. Send ACK with the next SEQ number expected
         else:
-            print('Successfully received SEQ #%d. Sending ACK #%d' % (seq, expectedPacket))
+            print('Successfully received SEQ #%d. Sending ACK #%d' % (SEQ, expectedPacket), "\n")
             pkt = packet.make(expectedPacket)
             udt.send(pkt, sock, senderaddr)
             expectedPacket += 1
 
+            # Makes sure that the FIN packet is not added to the text
             if endStr != 'END':
                 file.write(data.decode())
 
+    print('FIN has been ACK... Ending connection')
 
 # Receive packets from the sender w/ SR protocol
 def receive_sr(sock, windowsize):
@@ -44,39 +51,65 @@ def receive_snw(sock):
     endStr = ''
     expectedPacket = 0
 
+    # While FIN packet has not been received
     while endStr != 'END':
         pkt, senderaddr = udt.recv(sock)
-        seq, data = packet.extract(pkt)
+        SEQ, data = packet.extract(pkt)
         endStr = data.decode()
-        print("From: ", senderaddr, ", Seq# ", seq)
+        print("From: ", senderaddr, ", SEQ# ", SEQ)
 
-        if expectedPacket != seq:
-            print('This packet was not expected. Sending ACK %d' % expectedPacket)
+        # The received packet was not expected. Send ACK with the correct SEQ number
+        if expectedPacket != SEQ:
+            print('This packet was not expected. Sending ACK %d' % expectedPacket, "\n")
             pkt = packet.make(expectedPacket)
             udt.send(pkt, sock, senderaddr)
 
+        # This packet was expected. Send ACK with the next SEQ number expected
         else:
-            print('Successfully received SEQ #%d. Sending ACK #%d' % (seq, expectedPacket))
+            print('Successfully received SEQ #%d. Sending ACK #%d' % (SEQ, expectedPacket), "\n")
             pkt = packet.make(expectedPacket)
             udt.send(pkt, sock, senderaddr)
             expectedPacket += 1
 
+            # Makes sure that the FIN packet is not added to the text
             if endStr != 'END':
                 file.write(data.decode())
 
+    print('FIN has been ACK... Ending connection')
 
 # Main function
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Expected filename as command line argument')
+
+    # Select a protocol
+    print('Enter protocol(1: SnW - 2: GBn): ')
+    protocol = input()
+
+    # Check the user input
+    if protocol is not '1' and protocol is not '2':
+        print('Invalid protocol... Terminating')
+        exit()
+
+    # Enter file name
+    print('Enter filename: ')
+    filename = input()
+
+    # Check the user input
+    if len(filename) != 1 and '.txt' not in filename:
+        print('Invalid input file... Terminating')
         exit()
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(RECEIVER_ADDR)
 
-    filename = sys.argv[1]
     file = open(filename, 'w')
-    receive_snw(sock)
+
+    # Starts Stop-and-Wait receiver
+    if protocol is '1':
+        receive_snw(sock)
+
+    # Starts Go-Back-N receiver
+    elif protocol is '2':
+        receive_gbn(sock)
 
     # Close the socket
     sock.close()
--- Sender.py
+++ Sender.py
@@ -1,10 +1,14 @@
+# Team 16
+# Assignment 02: Reliable Data Transfer
+# October 09, 2020
+# CS5313 - Computer Networks
+
 import socket
-import sys
 import _thread
 import packet
 import udt
-from timer import Timer
 import time
+from timer import Timer
 
 # Some already defined parameters
 PACKET_SIZE = 512
@@ -22,17 +26,26 @@ timer = Timer(TIMEOUT_INTERVAL)
 
 # Need to have two threads: one for sending and another for receiving ACKs
 
-# Generate random payload of any length
-def generate_payload(length=10):
+# Generate payload for snw protocol using the given length and the initial byte of the packet
+def generate_payload_snw(length):
     global base
     initial_byte = PACKET_SIZE * base
     result_str = ''.join(bio[i] for i in range(initial_byte, initial_byte + length))
     return result_str
 
 
-def total_seq(length):
+# Generate payload for gbn protocol using the given length and the initial byte of the packet
+def generate_payload_gbn(length, SEQN):
+    initial_byte = PACKET_SIZE * SEQN
+    result_str = ''.join(bio[i] for i in range(initial_byte, initial_byte + length))
+    return result_str
+
+
+# Calculates how many SEQ are going to be sent by using the length of the file
+def total_SEQ(length):
     count = 0
 
+    # Calculates how many times length can be divided by PACKET_SIZE
     while True:
         num, remainder = divmod(length, PACKET_SIZE)
 
@@ -42,7 +55,7 @@ def total_seq(length):
         else:
             count += 1
 
-    return num + 1
+    return num
 
 
 # Send using Stop_n_wait protocol
@@ -50,43 +63,64 @@ def send_snw(sock):
     global base
     global mutex
     global timer
-    last_seq = total_seq(len(bio))
+
+    # Last SEQ to be sent
+    last_SEQ = total_SEQ(len(bio))
+
+    # Starts the receiving thread
     _thread.start_new_thread(receive_snw, (sock,))
 
-    while base < last_seq:
+    # Auxiliary variable for FIN
+    rep = 0
+
+    # Sends packets until is done
+    while base <= last_SEQ + 1:
         mutex.acquire()
 
-        if base == last_seq - 1:
-            data = generate_payload(len(bio) - (PACKET_SIZE * base)).encode()
+        # Sends FIN 6 times if FIN ACK was not received. Ensures that receiver receives FIN packet
+        if rep == 6:
+            break
 
-        elif base == last_seq:
-            print('Transfer complete... Sending FIN\n')
+        # Last part of the text is going to be sent
+        if base == last_SEQ - 1:
+            data = generate_payload_snw(len(bio) - (PACKET_SIZE * base)).encode()
+
+        # The text was sent. Time to send FIN to end the connection
+        elif base == last_SEQ:
+            print('Transfer complete... Sending FIN', "\n")
             data = "END".encode()
+            rep += 1
 
+        # FIN has been received... Ending connection
+        elif base == last_SEQ + 1:
+            break
+
+        # Regular packet is going to be sent
         else:
-            data = generate_payload(PACKET_SIZE).encode()
+            data = generate_payload_snw(PACKET_SIZE).encode()
+
         pkt = packet.make(base, data)
         print('Sending SEQ #', base, "\n")
         udt.send(pkt, sock, RECEIVER_ADDR)
 
+        # Check if timer is running. Start if is not running
         if not timer.running():
             timer.start()
 
+        # Waits 0.05 seconds and checks for the timer
         while timer.running() and not timer.timeout():
             mutex.release()
             time.sleep(SLEEP_INTERVAL)
             mutex.acquire()
 
+        # Waiting time exceeded... Time out
         if timer.timeout():
-            print('===================TIMEOUT====================')
+            print('Time out', "\n")
             timer.stop()
 
         mutex.release()
 
-    print('Transfer complete... Sending FIN\n')
-    data = "END".encode()
-    pkt = packet.make(base, data)
-    udt.send(pkt, sock, RECEIVER_ADDR)
+    print('FIN has been received... Ending connection')
 
 
 # Send using GBN protocol
@@ -94,44 +128,83 @@ def send_gbn(sock):
     global base
     global mutex
     global timer
-    last_seq = total_seq(len(bio))
-    index = 0
+
+    # Last SEQ to be sent
+    last_SEQ = total_SEQ(len(bio))
+
+    # Current SEQ
+    SEQN = 0
+
+    # Starts the receiving thread
     _thread.start_new_thread(receive_gbn, (sock,))
 
-    while base < last_seq:
+    # Auxiliary variable for FIN
+    rep = 0
+
+    # Sends packets until is done
+    while base <= last_SEQ + 1:
         mutex.acquire()
 
-        while index < base + WINDOW_SIZE:
+        # Check the window size and remaining packets
+        window = min(WINDOW_SIZE, last_SEQ - base)
 
-            if base == last_seq - 1:
-                data = generate_payload(len(bio) - (PACKET_SIZE * base)).encode()
+        # Send packets until it reach the limit of the window size
+        while SEQN <= base + (window - 1):
 
-            elif base == last_seq:
-                print('Transfer complete... Sending FIN\n')
-                data = "END".encode()
+            # Last part of the text is going to be sent
+            if SEQN == last_SEQ - 1:
+                data = generate_payload_gbn(len(bio) - (PACKET_SIZE * SEQN), SEQN).encode()
 
+            # Last packet was sent
+            elif SEQN == last_SEQ:
+                break
+
+            # Regular packet is going to be sent
             else:
-                data = generate_payload(PACKET_SIZE).encode()
+                data = generate_payload_gbn(PACKET_SIZE, SEQN).encode()
 
-            pkt = packet.make(base, data)
-            print('Sending SEQ #', base, "\n")
+            pkt = packet.make(SEQN, data)
+            print('Sending SEQ #', SEQN, "\n")
             udt.send(pkt, sock, RECEIVER_ADDR)
 
-            index += 1
+            SEQN += 1
 
+        # Sends FIN 6 times if FIN ACK was not received. Ensures that receiver receives FIN packet
+        if rep == 6:
+            break
+
+        # The text was sent. Time to send FIN to end the connection
+        if base == last_SEQ:
+            print('Transfer complete... Sending FIN', "\n")
+            data = "END".encode()
+            pkt = packet.make(SEQN, data)
+            print('Sending SEQ #', SEQN, "\n")
+            udt.send(pkt, sock, RECEIVER_ADDR)
+            rep += 1
+
+        # FIN has been received... Ending connection
+        elif base == last_SEQ + 1:
+            break
+
+        # Check if timer is running. Start if is not running
         if not timer.running():
             timer.start()
 
+        # Waits 0.05 seconds and checks for the timer
         while timer.running() and not timer.timeout():
             mutex.release()
             time.sleep(SLEEP_INTERVAL)
             mutex.acquire()
 
+        # Waiting time exceeded... Time out
         if timer.timeout():
-            print('===================TIMEOUT====================')
+            print('Time out', "\n")
             timer.stop()
+            SEQN = base
 
         mutex.release()
+
+    print('FIN has been received... Ending connection')
 
 
 # Receive thread for stop-n-wait
@@ -140,12 +213,14 @@ def receive_snw(sock):
     global mutex
     global timer
 
-    while base < total_seq(len(bio)):
+    # Receives ACK until the last one was received
+    while base <= total_SEQ(len(bio)):
         pkt, senderaddr = udt.recv(sock)
-        ack, data = packet.extract(pkt)
-        print('Received ACK #', ack, "\n")
+        ACK, data = packet.extract(pkt)
+        print('Received ACK #', ACK, "\n")
 
-        if ack >= base:
+        # Updates base if ACK is equal or greater than base
+        if ACK >= base:
             mutex.acquire()
             base += 1
             timer.stop
@@ -158,14 +233,23 @@ def receive_gbn(sock):
     global mutex
     global timer
 
-    while base < total_seq(len(bio)):
+    # Receives ACK until the last one was received
+    while base <= total_SEQ(len(bio)):
         pkt, senderaddr = udt.recv(sock)
-        ack, data = packet.extract(pkt)
-        print('Received ACK #', ack, "\n")
+        ACK, data = packet.extract(pkt)
+        print('Received ACK #', ACK, "\n")
 
-        if ack >= base:
+        # Updates base if ACK is equal or greater than base(Slide window)
+        if ACK >= base:
             mutex.acquire()
-            base = ack + 1
+            base = ACK + 1
+            timer.stop
+            mutex.release()
+
+        # The packets being sent are not expected. Updates base with the expected SEQ
+        else:
+            mutex.acquire()
+            base = ACK
             timer.stop
             mutex.release()
 
@@ -173,16 +257,36 @@ def receive_gbn(sock):
 # Main function
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        print('Expected filename as command line argument')
+    # Select a protocol
+    print('Enter protocol(1: SnW - 2: GBn): ')
+    protocol = input()
+
+    # Check the user input
+    if protocol is not '1' and protocol is not '2':
+        print('Invalid protocol... Terminating')
+        exit()
+
+    # Enter file name
+    print('Enter filename: ')
+    filename = input()
+
+    # Check the user input
+    if len(filename) != 1 and '.txt' not in filename:
+        print('Invalid input file... Terminating')
         exit()
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(SENDER_ADDR)
 
-    filename = sys.argv[1]
     file = open(filename, 'r')
     bio = file.read()
 
-    send_snw(sock)
+    # Starts Stop-and-Wait sender
+    if protocol is '1':
+        send_snw(sock)
+
+    # Starts Go-Back-N sender
+    elif protocol is '2':
+        send_gbn(sock)
+
     sock.close()
